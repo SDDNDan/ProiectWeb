2bc56de5a82bc0ac2c3b5232e9dc1ac2d9dfae52
DESKTOP-F9IPFTE\Bog <bgaidur@gmail.com>
2018-05-27 21:25:00 +0300

    Functional buttons in Manage Students page.

--- scripts/manageStudents.js
+++ scripts/manageStudents.js
@@ -3,3 +3,49 @@
  */
 
 
+let buttonAddGrade = document.getElementById("grade");
+let buttonAddPresence = document.getElementById("presence");
+let buttonAddBonus = document.getElementById("bonus");
+
+
+
+setAllInvisible();
+
+function setAllInvisible() {
+    document.getElementsByClassName("add__grade__container")[0].style.display = "none";
+    document.getElementsByClassName("add__presence__container")[0].style.display = "none";
+    document.getElementsByClassName("add__bonus__container")[0].style.display = "none";
+}
+
+buttonAddGrade.addEventListener("click", function () {
+    if (document.getElementsByClassName("add__grade__container")[0].style.display === "none") {
+        setAllInvisible();
+        document.getElementsByClassName("add__grade__container")[0].style.display = "block";
+    }
+    else {
+        setAllInvisible();
+        buttonAddGrade.blur();
+    }
+});
+
+buttonAddPresence.addEventListener("click", function () {
+    if (document.getElementsByClassName("add__presence__container")[0].style.display === "none") {
+        setAllInvisible();
+        document.getElementsByClassName("add__presence__container")[0].style.display = "block";
+    }
+    else {
+        setAllInvisible();
+        buttonAddPresence.blur();
+    }
+});
+
+buttonAddBonus.addEventListener("click", function () {
+    if (document.getElementsByClassName("add__bonus__container")[0].style.display === "none") {
+        setAllInvisible();
+        document.getElementsByClassName("add__bonus__container")[0].style.display = "block";
+    }
+    else {
+        setAllInvisible();
+        buttonAddBonus.blur();
+    }
+});
