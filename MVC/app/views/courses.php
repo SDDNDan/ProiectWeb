9364f7c870fa844cb3be2948330ec5cf8b67965a
DESKTOP-VIVS3E9\Stefan <stftimofte@gmail.com>
2018-06-11 14:11:34 +0300

    Merge branch 'master' of https://github.com/SDDNDan/ProiectWeb
    # Conflicts:
    #	MVC/app/views/GradeStatistics.php

--- MVC/app/views/courses.php
+++ MVC/app/views/courses.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <title>Web Technologies App</title>
-    <?php include VIEWS.'partials/base_url.php' ?>
-    <link rel="stylesheet" type="text/css" href="styles/style.css"/>
+  <?php include VIEWS.'partials/base_url.php' ?>
+    <link rel="stylesheet" type="text/css" href="../../public_html/styles/style.css"/>
 </head>
 <body>
     <header>
         <h1 class="index__title">Web Technologies App</h1>
         <div class="content">
-            <?php require_once("partials/nav.php"); ?>
+           <?php require_once("partials/nav.php"); ?>
         </div>
         <div class = "dropdown">
             <button class="dropdown__button">
@@ -27,7 +27,7 @@
 
     <section class="p-top-60">
         <div class="courses__page">
-        <h1 class="section__title">Courses & Laboratories</h1>
+        <h1 class="section__title" style = "padding-top : 10%; padding-bottom : 7%; ">Courses & Laboratories</h1>
 
             <div class = cursSiLaborator>
                 <div class = courseColoumn>
@@ -35,7 +35,7 @@
                     <div class = courseName>Concepte si viziune primara</div>
                 </div>
                 <div class = laboratorColoumn>
-                    <button class = laboratorButton onclick="window.location.href='http://slides.com/iampava/tw#/'" >Lab #1</button>
+                    <button class = laboratorButton onclick="window.location.href='https://slides.com/iampava/tw#/'" >Lab #1</button>
                 </div>
             </div>
 
@@ -45,7 +45,7 @@
                     <div class = courseName>Programare Web</div>
                 </div>
                 <div class = laboratorColoumn>
-                    <button class = laboratorButton onclick="window.location.href='http://slides.com/iampava/tw#/13'">Lab #2</button>
+                    <button class = laboratorButton onclick="window.location.href='https://slides.com/iampava/tw#/13'">Lab #2</button>
                 </div>
             </div>
 
@@ -55,7 +55,7 @@
                     <div class = courseName>Ingineria Web</div>
                 </div>
                 <div class = laboratorColoumn>
-                    <button class = laboratorButton onclick="window.location.href='https://iampava.com/tw/'">Lab #3</button>
+                    <button class = laboratorButton onclick="window.location.href='https://slides.com/iampava/tw#/25'">Lab #3</button>
                 </div>
             </div>
 
@@ -78,6 +78,88 @@
                     <button class = laboratorButton onclick = "window.location.href = 'https://iampava.com/tw/'">Lab #5</button>
                 </div>
             </div>
+
+
+            <div class = cursSiLaborator>
+                <div class = courseColoumn>
+                    <button class = courseButton onclick="https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web06ModelareXML-FamiliaXML.pdf">Course 6</button>
+                    <div class = courseName>Familia XML</div>
+                </div>
+                <div class = laboratorColoumn>
+                    <button class = laboratorButton onclick = "window.location.href = 'https://slides.com/iampava/tw#/47'">Lab #6</button>
+                </div>
+            </div>
+
+
+            <div class = cursSiLaborator>
+                <div class = courseColoumn>
+                    <button class = courseButton onclick="window.location.href='https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web07ModelareXML-XPath-ValidariXML-DTD.pdf'">Course 7</button>
+                    <div class = courseName>Familia XML II</div>
+                </div>
+                <div class = laboratorColoumn>
+                    <button class = laboratorButton onclick = "window.location.href = 'https://slides.com/iampava/tw#/57'">Lab #7</button>
+                </div>
+            </div>
+
+
+            <div class = cursSiLaborator>
+                <div class = courseColoumn>
+                    <button class = courseButton onclick="window.location.href='https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web08ProcesariXML-DOM.pdf'">Course 9</button>
+                    <div class = courseName>Procesadea datelor XML - DOM </div>
+                </div>
+                <div class = laboratorColoumn>
+                    <button class = laboratorButton onclick="window.location.href='https://slides.com/iampava/tw#/68'" >Lab #9</button>
+                </div>
+            </div>
+
+
+             <div class = cursSiLaborator>
+                 <div class = courseColoumn>
+                     <button class = courseButton onclick="window.location.href='https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web09ProcesariXML-SAX-SimpleXML.pdf'">Course 10</button>
+                     <div class = courseName>Prelucrarea simplificata a documentelor XML</div>
+                 </div>
+                 <div class = laboratorColoumn>
+                     <button class = laboratorButton onclick="window.location.href='https://slides.com/iampava/tw#/80'" >Lab #10</button>
+                 </div>
+             </div>
+
+
+            <div class = cursSiLaborator>
+                <div class = courseColoumn>
+                    <button class = courseButton onclick="window.location.href='https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web10ServiciiWeb-SOA-SOAP-WSDL-UDDI.pdf'">Course 11</button>
+                    <div class = courseName>Servicii Web - De la SOA la SOAP</div>
+                </div>
+                <div class = laboratorColoumn>
+                    <button class = laboratorButton onclick="window.location.href='http://slides.com/iampava/tw#/'" >Lab #11</button>
+                </div>
+            </div>
+
+
+
+            <div class = cursSiLaborator>
+                <div class = courseColoumn>
+                    <button class = courseButton onclick="window.location.href='https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web11ServiciiWeb-REST.pdf'">Course 12</button>
+                    <div class = courseName>Paradigma REST</div>
+                </div>
+                <div class = laboratorColoumn>
+                    <button class = laboratorButton onclick="window.location.href='http://slides.com/iampava/tw#/91'" >Lab #12</button>
+                </div>
+            </div>
+
+
+
+            <div class = cursSiLaborator>
+                <div class = courseColoumn>
+                    <button class = courseButton onclick="window.location.href='https://profs.info.uaic.ro/~busaco/teach/courses/web/presentations/web12InteractiuneWeb-Ajax-Mashups.pdf'">Course 13</button>
+                    <div class = courseName>Microservicii si interactiune Web </div>
+                </div>
+                <div class = laboratorColoumn>
+                    <button class = laboratorButton onclick="window.location.href='http://slides.com/iampava/tw#/99'" >Lab #13</button>
+                </div>
+            </div>
+
+
+
 
 
         </div>
