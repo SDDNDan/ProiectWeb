1aa0dcd2e2970e2b21a3f0fb593dd65104900c82
DESKTOP-F9IPFTE\Bog <bgaidur@gmail.com>
2018-06-12 10:29:04 +0300

    Minor fixes to main page.

--- MVC/app/views/index.php
+++ MVC/app/views/index.php
@@ -11,7 +11,7 @@ if (session_status() == PHP_SESSION_NONE) {
     <?php include VIEWS.'partials/base_url.php' ?>
 
 
-    <link rel="stylesheet" type="text/css" href="styles/login_page.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/style.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body id="indexbody">
